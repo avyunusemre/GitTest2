52ec9b75d90749a9c5c483ef17bc8616063b6a08
avyunusemre <67027810+avyunusemre@users.noreply.github.com>
2021-05-02 20:10:44 +0200

    update Overloading

--- src/Overloading.java
+++ src/Overloading.java
@@ -3,5 +3,7 @@ public class Overloading {
     public static void main(String[] args) {
 
         System.out.println("Yeni class");
+        
+        System.out.println("sena'nın kodu");
     }
 }
